357a6577162c0c312affbff4d50770b6269b1504
patrick tettenborn <pat@tetten.com>
2019-11-03 18:20:49 +0100

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  publicPath: '/theme/umsichten-2019/dist'
+  publicPath: '/theme/umsichten-2019/dist',
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "@/scss/_variables.scss"; 
+          @import "@/scss/_mixins.scss";
+          @import "@/scss/_globals.scss";
+          @import "@/scss/_animations.scss";
+          @import "@/scss/_typo.scss";
+        `
+      }
+    }
+  }
 }
